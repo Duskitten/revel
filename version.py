92d79b5c99868358fe89e111e372a1878126506b
Duskitty <Theduskitty@gmail.com>
2024-08-16 03:32:04 -0400

    Adj

--- version.py
+++ version.py
@@ -1,15 +1,9 @@
 short_name = "revel"
 name = "Revel Engine"
 major = 4
-<<<<<<< HEAD
-minor = 3
-patch = 0
-status = "rc"
-=======
 minor = 2
 patch = 6
 status = "beta"
->>>>>>> aba7ebe27a80cf01796d140ec6b0987b0d0d5913
 module_config = ""
 website = "https://godotengine.org"
 docs = "latest"
